a2a602dfa86a1c9b060ef60ab40983dbbd081d06
aveluru <aveluru@nextgen.com>
2019-08-20 12:46:40 +0530

    First Commit

---
+++ src/main/java/com/example/restservices/SpringBootExampleRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleRestapiApplication.class, args);
+	}
+
+}
